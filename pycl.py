2b80de56042f12550e5e9e112b227159178b4d9e
a-slide <aleg@ebi.ac.uk>
2016-06-21 18:24:49 +0100

    Add more advanced bash cl options

--- pycl.py
+++ pycl.py
@@ -297,12 +297,93 @@ def mkdir(fp):
 
 #~~~~~~~ SHELL MANIPULATION ~~~~~~~#
 
-def bash(cmd):
+def make_cmd_str(prog_name, opt_dict={}, opt_list=[]):
+    """
+    Create a Unix like command line string from a
+    @param prog_name Name (if added to the system path) or path of the programm
+    @param opt_dict Dictionnary of option arguments such as "-t 5". The option flag have to
+    be the key (without "-") and the the option value in the dictionnary value. If no value is
+    requested after the option flag "None" had to be asigned to the value field.
+    @param opt_list List of simple command line arguments
+    @exemple make_cmd_str("bwa", {"b":None, t":6, "i":"../idx/seq.fa"}, ["../read1", "../read2"])
+    """
+    
+    # Start the string by the name of the program
+    cmd = "{} ".format(prog_name)
+
+    # Add options arguments from opt_dict
+    if opt_dict:
+        for key, value in opt_dict.items():
+            if value:
+                cmd += "{} {} ".format(key, value)
+            else:
+                cmd += "{} ".format(key)
+
+    # Add arguments from opt_list
+    if opt_list:
+        for value in opt_list:
+            cmd += "{} ".format(value)
+
+    return cmd
+
+
+def bash_basic(cmd):
     """Sent basic bash command"""
     process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
     stdout, stderr = process.communicate()
     print(stdout.decode())
     print (stderr.decode())
+
+
+def bash(cmd, stdin=None, ret_stderr=False, ret_stdout=True, str_output=True):
+    """
+    Run a command line in the default shell and return the standard output
+    @param  cmd A command line string formated as a string
+    @param  stdinput    Facultative parameters to redirect an object to the standard input
+    @param  ret_stderr  If True the standard error output will be returned
+    @param  ret_stdout  If True the standard output will be returned
+    @param  str_output  Transform the std output in a string instead of the bytes-like object
+    @note If ret_stderr and ret_stdout are True a tuple will be returned and if both are False
+    None will be returned
+    @return If no standard error return the standard output as a string
+    @exception  OSError Raise if a message is return on the standard error output
+    @exception  (ValueError,OSError) May be raise by Popen
+    """
+
+    # Execute the command line in the default shell
+    if stdin:
+        proc = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = proc.communicate(input=stdin)
+    else:
+        proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = proc.communicate()
+
+    if proc.returncode == 1:
+        msg = "An error occured during execution of following command :\n"
+        msg += "COMMAND : {}\n".format(cmd)
+        msg += "STDERR : {}\n".format(stderr.decode())
+        print (msg)
+        return None
+
+    # Else return data according to user choices is returned
+    if str_output:
+        if ret_stdout and ret_stderr:
+            return stdout.decode(), stderr.decode()
+        elif ret_stdout:
+            return stdout.decode()
+        elif ret_stderr:
+            return stderr.decode()
+        
+    else:
+        if ret_stdout and ret_stderr:
+            return stdout, stderr
+        elif ret_stdout:
+            return stdout
+        elif ret_stderr:
+            return stderr
+   
+    # Last possibility
+    return None
 
 ##~~~~~~~ DICTIONNARY FORMATTING ~~~~~~~#
 
